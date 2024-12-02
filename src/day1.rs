26adb01e5e6d6375671c517b9ea12b6d64b36f13
SenseiSamy <samy.naji2003@gmail.com>
2024-12-02 23:57:30 +0100

    day 1

---
+++ src/day1.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+#[aoc_generator(day1)]
+pub fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+	let mut list1 = Vec::new();
+	let mut list2 = Vec::new();
+	
+	for line in input.lines() {
+		let mut iter = line.split_whitespace();
+		list1.push(iter.next().unwrap().parse().unwrap());
+		list2.push(iter.next().unwrap().parse().unwrap());
+	}
+	(list1, list2)
+}
+
+#[aoc(day1, part1)]
+fn part1(lists: &(Vec<i32>, Vec<i32>)) -> i32 {
+	let (mut list1, mut list2) = lists.clone();
+	list1.sort();
+	list2.sort();
+	list1.iter()
+		.zip(list2.iter())
+		.map(|(a, b)| (a - b).abs())
+		.reduce(|acc, e| acc + e)
+		.unwrap()
+}
+
+#[aoc(day1, part2)]
+fn part2(lists: &(Vec<i32>, Vec<i32>)) -> i32 {
+	let (list1, list2) = lists.clone();
+	let mut res = 0;
+	let mut map: HashMap<i32, i32> = HashMap::new();
+	for n in list2 {
+		map.entry(n)
+			.and_modify(|e| *e += 1)
+			.or_insert(1);
+	}
+	for n in list1 {
+		res += n * map.get(&n).unwrap_or(&0);
+	}
+	res
+}
